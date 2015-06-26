bd1513c827eab2b44dac29b103ab3b01f62179f0
Matthew Beckler <matthew.beckler@gmail.com>
2015-06-26 15:52:58 -0500

    Updated BricktronicsSettings.h utility file comments

--- utility/BricktronicsSettings.h
+++ utility/BricktronicsSettings.h
@@ -1,36 +1,37 @@
 /*
-    Struct definitions for motor and sensor settings.
-    Provides a compact way to transfer motor and sensor settings
-    from the Bricktronics class to the motor and sensor class constructors.
+   BricktronicsSettings v1.2
+   Struct definitions for motor and sensor settings.
+   Provides a compact way to transfer motor and sensor settings
+   from the BricktronicsShield / BricktronicsMegashield class to
+   the motor and sensor class constructors.
 
-    Copyright (C) 2014 Adam Wolf, Matthew Beckler, John Baichtal
+   Copyright (C) 2015 Adam Wolf, Matthew Beckler, John Baichtal
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation; either version 2
-    of the License, or (at your option) any later version.
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-    Wayne and Layne invests time and resources providing this open-source
-    code, please support W&L and open-source hardware by purchasing products
-    from https://store.wayneandlayne.com/ - Thanks!
+   Wayne and Layne invests time and resources providing this open-source
+   code, please support W&L and open-source hardware by purchasing products
+   from https://store.wayneandlayne.com/ - Thanks!
 
-    Wayne and Layne, LLC and our products are not connected to or endorsed by the LEGO Group.
-    LEGO, Mindstorms, and NXT are trademarks of the LEGO Group.
+   Wayne and Layne, LLC and our products are not connected to or endorsed by the LEGO Group.
+   LEGO, Mindstorms, and NXT are trademarks of the LEGO Group.
 */
 
 #ifndef BRICKTRONICSSETTINGS_H
 #define BRICKTRONICSSETTINGS_H
 
-// Arduino header files
 #include <inttypes.h>
 
 typedef struct BricktronicsMotorSettings
